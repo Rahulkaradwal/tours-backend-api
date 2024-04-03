d290047ccd42570f158d0a2c39ffd54e5ac680f9
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-04-02 20:46:34 -0400

    feat: intial route to get the tours from json file

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const { dirname } = require('path');
 const app = express();
-
 const port = 3000;
+const fs = require('fs');
+
+const tours = JSON.parse(fs.readFileSync('tours-simple.json'));
+
+app.get('/api/tours', (req, res) => {
+  res.status(200).json({
+    data: { tours },
+  });
+});
 
 app.listen(port, () => {
   console.log('Server is running on port :', port);
