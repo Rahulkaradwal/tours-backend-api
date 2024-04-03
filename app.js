28e4cfe51ec6bf88a2bda8a34c65119ef3d138ed
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-04-02 21:01:52 -0400

    feat: get and post route for tours

--- app.js
+++ app.js
@@ -6,9 +6,28 @@ const fs = require('fs');
 
 const tours = JSON.parse(fs.readFileSync('tours-simple.json'));
 
+app.use(express.json());
+
 app.get('/api/tours', (req, res) => {
   res.status(200).json({
+    totalResults: tours.length,
     data: { tours },
+  });
+});
+
+app.post('/api/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile('tours-simple.json', JSON.stringify(tours), (err) => {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
   });
 });
 
