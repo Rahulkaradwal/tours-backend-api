7423289cc2a8fc9dabaac138940556188ef260ec
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-04-02 22:49:45 -0400

    feat: add update, delete routes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const tours = JSON.parse(fs.readFileSync('tours-simple.json'));
 
 app.use(express.json());
 
+// get all tours
 app.get('/api/tours', (req, res) => {
   res.status(200).json({
     totalResults: tours.length,
@@ -15,6 +16,23 @@ app.get('/api/tours', (req, res) => {
   });
 });
 
+// get a tour
+app.get('/api/tours/:id', (req, res) => {
+  const id = req.params.id * 1; // to convert string into number we mulitplied by number
+
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    res.status(404).json({
+      status: 'Fail',
+      message: 'Invlid ID',
+    });
+  }
+  res.status(200).json({
+    data: { tour },
+  });
+});
+
+// add a tour
 app.post('/api/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
@@ -28,6 +46,35 @@ app.post('/api/tours', (req, res) => {
         tour: newTour,
       },
     });
+  });
+});
+
+// update a tour
+app.patch('/api/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    data: { tour },
+  });
+});
+
+// delete a tour
+app.delete('/api/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: null,
   });
 });
 
