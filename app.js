09bd499fbcde8a8c42004597e9904e4d07dafbf6
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-04-04 19:59:26 -0400

    feat: add mongo db connection

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
 
 const tourRouter = require('./routes/tourRoute');
 const userRouter = require('./routes/userRoute');
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+const port = process.env.PORT || 8000;
+
 const app = require('./app');
-const port = 3000;
 
 app.listen(port, () => {
   console.log('Server is running on port :', port);
