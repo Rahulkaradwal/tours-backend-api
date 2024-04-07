b06fbda7abf84d8b66abf770bd96f787129f2663
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-04-07 19:58:47 -0400

    feat: middleware to handle global error

--- app.js
+++ app.js
@@ -16,4 +16,30 @@ const userRouter = require('./routes/userRoute');
 app.use('/api/tours', tourRouter);
 app.use('/api/users', userRouter);
 
+app.use('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find the requested url ${req.originalUrl}, please try with correct url`,
+  // });
+  // next();
+
+  const err = new Error(
+    `Can't find the requested url ${req.originalUrl}, please try with correct url`
+  );
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
+
+// global error handler middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
+
 module.exports = app;
