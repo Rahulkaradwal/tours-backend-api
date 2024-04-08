6e842b7cdc59b249930a7dea9fcea0cf4f705d44
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-04-08 19:29:52 -0400

    feat: add user model

---
+++ models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide your name'],
+  },
+
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please privode a valid email'],
+  },
+  photo: { type: String },
+  password: {
+    type: String,
+    required: [true, 'Pleaes enter your Password'],
+    minlength: 8,
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, 'Pleaes enter correct Password'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+exports.module = User;
