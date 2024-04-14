a5140c0a705b37ce09793e75c4bca8f062a3297f
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-04-14 03:43:58 -0400

    feat: add functoinality to delete the current user

--- controller/userController.js
+++ controller/userController.js
@@ -97,3 +97,14 @@ exports.updateMe = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, {
+    active: false,
+  });
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -14,6 +14,7 @@ router.patch(
   authController.updatePassword
 );
 router.post('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
 router
   .route('/')
