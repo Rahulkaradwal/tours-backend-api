3a0040d4d0f2e4fdd0066e90d6bfb2658c5e37e8
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-04-14 04:12:25 -0400

    feat: add xss and sanitization

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const AppError = require('./utils/AppError');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000,
@@ -17,6 +19,9 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(helmet());
 app.use(express.json());
+
+app.use(mongoSanitize());
+app.use(xss());
 
 app.use(express.static(`${__dirname}/public`));
 
