dc317a528af7223e33f693e751fdaca49a48c179
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-04-15 03:48:41 -0400

    refactor: populating middleware for review

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -34,7 +34,10 @@ const reviewSchema = new mongoose.Schema(
 );
 
 reviewSchema.pre(/^find/, function (next) {
-  this.populate({ path: 'tour user' });
+  this.populate({ path: 'tour', select: 'name' }).populate({
+    path: 'user',
+    select: 'name photo',
+  });
   next();
 });
 
