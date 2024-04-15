6ea2c6e46032e026799ee5957f0c2339ed470b8d
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-04-15 01:58:38 -0400

    refactor: review model

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,29 +1,37 @@
 const mongoose = require('mongoose');
 
-const reviewSchema = new mongoose.Schema({
-  review: {
-    type: String,
-    required: [true, 'A review must be there'],
-    trim: true,
-  },
-  rating: {
-    type: Number,
-    required: [true, 'A tour must have a rating'],
-  },
-  cratedAt: Date,
-  tour: [
-    {
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review can not be empty'],
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    cratedAt: {
+      type: Date,
+      default: Date.now,
+    },
+    tour: {
       type: mongoose.Schema.ObjectId,
       ref: 'Tour',
+      required: [true, 'Review must belong to a tour'],
     },
-  ],
-  user: [
-    {
+
+    user: {
       type: mongoose.Schema.ObjectId,
       ref: 'User',
+      required: [true, 'Review must belong to a user'],
     },
-  ],
-});
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 const Review = mongoose.model('Review', reviewSchema);
 module.exports = Review;
