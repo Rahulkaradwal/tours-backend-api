1306cd8d267fc1bfd9c4d873be20e884d214f4da
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-05-20 18:37:57 -0400

    fix: multer config

--- controller/userController.js
+++ controller/userController.js
@@ -20,14 +20,15 @@ const multerStorage = multer.diskStorage({
     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
   },
 });
-
-const multerFilter = (req, res, cb) => {
+const multerFilter = (req, file, cb) => {
+  // Corrected signature here
   if (file.mimetype.startsWith('image')) {
     cb(null, true);
   } else {
     cb(new AppError('Not an image! Please upload only images', 400), false);
   }
 };
+
 const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
 
 exports.uploadUserPhoto = upload.single('photo');
